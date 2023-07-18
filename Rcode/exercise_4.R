8222f019ea5fa9abb6c3a453c39a6da1dbde2957
mtarruella7 <90260246+mtarruella7@users.noreply.github.com>
2023-07-18 16:46:54 +0200

    Update exercise_4.R

--- Rcode/exercise_4.R
+++ Rcode/exercise_4.R
@@ -8,7 +8,9 @@ library(haven)
 library(AER)
 library(stargazer)
 
-
+#-------------------------------------------------------------------------------
+#### A) Instrumental-Variable: 
+#-------------------------------------------------------------------------------
 # set seed for reproducibility
 set.seed(123)
 
@@ -34,3 +36,96 @@ stargazer(iv_reg_reduced, iv_reg_first, iv_reg_2sls, out = "Tables/instrumental.
           )
 
  #add.lines = list(c("Fixed effects?", "No", "No"))
+
+#-------------------------------------------------------------------------------
+#### B) RDD
+#-------------------------------------------------------------------------------
+library(tidyverse)
+library(broom)
+library(rdrobust)
+library(rddensity)
+library(modelsummary)
+library(estimatr)
+
+full_data <- read.csv2("Data/dataset.csv")
+
+# Compliance histogram
+#-------------------------------------------------------------------------------
+distance_bins <- full_data %>%
+  mutate(distance_binned = cut(distance, breaks = seq(0, 1, 0.05))) %>%
+  # Group by each of the new bins and treatment  status
+  group_by(distance_binned, d_self_selection) %>%
+  # Count how many people are in each test bin + didn't self-select
+  summarize(n = n()) %>%
+  # Make this summarized data wider so that there's a column for treatment  and no treatment 
+  pivot_wider(names_from = "d_self_selection", values_from = "n", values_fill = 0) %>%
+  rename(treatment_yes = `1`, treatment_no = `0`) %>%
+  # Find the probability of treatment in each bin by taking
+  # the count of yes / count of yes + count of no
+  mutate(propoprtion_treatment = treatment_yes / (treatment_yes + treatment_no))
+
+
+ggplot(distance_bins, aes(x = distance_binned, y = propoprtion_treatment)) +
+  geom_col() +
+  geom_vline(xintercept = 0.3) +
+  labs(x = "Distance to Zürich", y = "Proportion of people participating in the German course")
+
+# Discontinuity in outcome across running variable
+#-------------------------------------------------------------------------------
+# Outcome: Income
+# Running variable: distance to ZCH
+# Cutoff value: 0.3 
+cutoff_value <- 0.3
+
+### FUZZY plot 
+
+full_data_rdd <- full_data[1:15000,] %>% #reducing the sample for visualization purposes
+  mutate(d_self_selection = factor(d_self_selection)) #factoring for the plot
+
+ggplot(full_data_rdd , aes(x= distance, y=income_fe_t1_self_selection, color = d_self_selection))+
+  geom_vline(xintercept = 0.4)+
+  geom_point(size = 0.75, alpha = 0.15) +
+  scale_color_manual(values = c("orange", "blue")) +
+  guides(color = guide_legend(override.aes = list(shape = c(15, 15))))+
+  geom_smooth(method = "lm", data = filter(full_data_rdd, d_self_selection == 1)) +
+  geom_smooth(method = "lm", data = filter(full_data_rdd, d_self_selection == 0))+
+  labs(x = "Distance to ZCH", y = "Income", color = "Selection to Treatment")
+
+### FUZZY plot around the cutoff value
+
+#Plot with regression of values around the cutoff
+ggplot(full_data_rdd , aes(x= distance, y=income_fe_t1_self_selection, color = d_self_selection))+
+  geom_vline(xintercept = 0.4)+
+  geom_point(size = 0.75, alpha = 0.15) +
+  scale_color_manual(values = c("orange", "blue")) +
+  guides(color = guide_legend(override.aes = list(shape = c(15, 15))))+
+  geom_smooth(method = "lm", data = filter(full_data_rdd,
+                                           distance >= 0.35,
+                                           distance < cutoff_value,
+                                           d_self_selection == 1)) +
+  geom_smooth(method = "lm", data = filter(full_data_rdd, 
+                                           distance > cutoff_value,
+                                           distance <= 0.45,
+                                           d_self_selection ==0))+
+  labs(x = "Distance to ZCH", y = "Income", color = "Selection to Treatment")
+
+
+# Size of the effect - Measuring the GAP
+#-------------------------------------------------------------------------------
+# Measure the GAP around the cutoff
+# Parametric method: regression
+# center our running variable around the cutoff 
+
+distance_centered <- full_data %>% 
+  mutate(distance_center = distance - cutoff_value,
+         below_cutoff = distance <= cutoff_value)
+
+# FUZZY assumption
+
+model_fuzzy <- iv_robust(income_fe_t1_self_selection ~  distance_center + d_self_selection | distance_center + below_cutoff,
+                         data = filter(distance_centered,
+                                       distance_center <= 0.05 &
+                                       distance_center >= -0.05))
+tidy(model_fuzzy)
+#Intercept: 4350 --> income at distance_center = 0 --> distance = cutoff_value & d_self_selection = 0
+#d_self_selection: 414 --> treatment jumps 414 from one group to another --> causal effect 
